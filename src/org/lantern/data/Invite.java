55aaed909070bac66ecfcf6a33a103bce15e03b1
Leah X Schmidt <leahxschmidt@gmail.com>
2013-02-14 21:43:56 -0500

    use different separator since 0 causes weirdness in the admin interface

--- src/org/lantern/data/Invite.java
+++ src/org/lantern/data/Invite.java
@@ -3,19 +3,22 @@ package org.lantern.data;
 import javax.persistence.Id;
 
 /**
- * An invite's id is inviter-email-address\invitee-email-address
+ * An invite's id is inviter-email-address\1invitee-email-address
  */
 public class Invite {
 
     @Id
-    private final String id;
+    private String id;
+
+    public Invite() {
+    };
 
     public Invite(String inviter, String invitee) {
         this.id = makeKey(inviter, invitee);
     }
 
     public static String makeKey(String inviterEmail, String inviteeEmail) {
-        return inviterEmail + "\0" + inviteeEmail;
+        return inviterEmail + "\1" + inviteeEmail;
     }
 
 }
