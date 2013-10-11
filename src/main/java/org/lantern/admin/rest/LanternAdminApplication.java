98c47954e203bde0924a0a58c01a257f19522d27
Ox To A Cart <ox.to.a.cart@gmail.com>
2013-10-11 17:04:01 -0500

    #945 Added support for JAX-RS using Jersey

---
+++ src/main/java/org/lantern/admin/rest/LanternAdminApplication.java
@@ -0,0 +1,29 @@
+package org.lantern.admin.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+/**
+ * <p>
+ * Big thanks to this project for providing a template on using Jersey with
+ * AppEngine.
+ * </p>
+ * 
+ * <p>
+ * https://github.com/GoogleCloudPlatform/appengine-angular-guestbook-java
+ * </p>
+ */
+public class LanternAdminApplication extends Application {
+    private Set<Class<?>> resources = new HashSet<Class<?>>();
+
+    public LanternAdminApplication() {
+        resources.add(LanternVersionResource.class);
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return resources;
+    }
+}
