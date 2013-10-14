d3640de7887e08de892ce023305d7ed01908af0d
Myles Horton <myles@getlantern.org>
2013-10-14 07:38:31 -0700

    Changed to load credentials from a file -- a little easier

--- src/test/java/org/lantern/RemoteApi.java
+++ src/test/java/org/lantern/RemoteApi.java
@@ -1,7 +1,13 @@
 package org.lantern;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.lantern.data.Dao;
 
 import com.google.appengine.tools.remoteapi.RemoteApiInstaller;
@@ -9,16 +15,36 @@ import com.google.appengine.tools.remoteapi.RemoteApiOptions;
 
 public class RemoteApi {
 
-    public static void main(final String[] args) {
-        String username = System.console().readLine("username: ");
-        String password = new String(System.console().readPassword("password: "));
-        RemoteApiOptions options = new RemoteApiOptions()
-            .server("lanternctrl.appspot.com", 443)
+    public static void main(final String[] args) throws IOException {
+        
+        final Properties props = new Properties();
+        final File creds = new File("remoteapi.properties");
+        if (creds.isFile()) {
+            InputStream is = null;
+            try {
+                is = new FileInputStream(creds);
+                props.load(is);
+            } finally {
+             IOUtils.closeQuietly(is);
+            }
+        } else {
+            System.err.println("PLEASE ENTER username: and password: and " +
+                "optionally controller: in "+creds.getAbsolutePath());
+            return;
+        }
+         
+        final String username = props.getProperty("username");
+        final String password = props.getProperty("password");
+        final String stored = props.getProperty("controller");
+        final String controller = 
+            StringUtils.isNotBlank(stored) ? stored + ".appspot.com" : "lanternctrl.appspot.com";
+        final RemoteApiOptions options = new RemoteApiOptions()
+            .server(controller, 443)
             .credentials(username, password);
-        RemoteApiInstaller installer = new RemoteApiInstaller();
+        final RemoteApiInstaller installer = new RemoteApiInstaller();
         try {
             installer.install(options);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
             return;
