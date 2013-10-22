752b6e5d98e8e3fd8dd9a5c54b436782958ff1de
Ox To A Cart <ox.to.a.cart@gmail.com>
2013-10-22 07:48:17 -0500

    #946 Added in binding of where clause

--- src/main/webapp/admin/invites.js
+++ src/main/webapp/admin/invites.js
@@ -1,26 +1,10 @@
 angular.module('InvitesApp', ['ngResource'])
   .run(function($resource, $rootScope) {
     var invitesResource = $resource('rest/invites/pending');
-    // Fetch the invites
-    var invites = invitesResource.query(function () {
-      // Organize them into a tree grouped by inviters
-      // Below is some sample data for testing locally
-      //invites = [{"id":"lanternfriend@gmail.com\u0001ox@getlantern.org","inviter":{"id":"lanternfriend@gmail.com","degree":2,"hasFallback":false,"countries":["US"],"sponsor":"lanternfriend@gmail.com"},"invitee":{"id":"ox@getlantern.org","degree":null,"hasFallback":null,"countries":null,"sponsor":null}}] 
-
-      var inviters = {};
-      invites.forEach(function(invite) {
-        var inviter = inviters[invite.inviter.id];
-        if (!inviter) {
-          inviter = invite.inviter;
-          inviter.invites = [];
-          inviters[inviter.id] = inviter;
-        }
-        inviter.invites.push(invite);
-      });
-      $rootScope.inviters = _.values(inviters);
-    });
-
+    
     $rootScope.where = '';
+    // At the moment, there is no way to change the ordering in the UI
+    $rootScope.ordering = 'inviter'
     $rootScope.allSelected = false;
     
     function setSelectionForAllInvites(inviter, selected) {
@@ -50,5 +34,28 @@ angular.module('InvitesApp', ['ngResource'])
     $rootScope.toggleAllForInviter = function (inviter) {
       setSelectionForAllInvites(inviter, !inviter.allSelected);
       reevaluateAllSelected();
+    };
+    
+    $rootScope.search = function() {
+      // Fetch the invites
+      var invites = invitesResource.query({where: $rootScope.where,
+                                           ordering: $rootScope.ordering},
+                                           function () {
+        // Organize them into a tree grouped by inviters
+        // Below is some sample data for testing locally
+        // invites = [{"id":"lanternfriend@gmail.com\u0001ox@getlantern.org","inviter":{"id":"lanternfriend@gmail.com","degree":2,"hasFallback":false,"countries":["US"],"sponsor":"lanternfriend@gmail.com"},"invitee":{"id":"ox@getlantern.org","degree":null,"hasFallback":null,"countries":null,"sponsor":null}}] 
+
+        var inviters = {};
+        invites.forEach(function(invite) {
+          var inviter = inviters[invite.inviter.id];
+          if (!inviter) {
+            inviter = invite.inviter;
+            inviter.invites = [];
+            inviters[inviter.id] = inviter;
+          }
+          inviter.invites.push(invite);
+        });
+        $rootScope.inviters = _.values(inviters);
+      });
     };
   })
