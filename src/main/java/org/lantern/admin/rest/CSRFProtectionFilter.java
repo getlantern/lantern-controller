249c8faa4d2d32b380f5acdb2a11085bd7a7e9a3
_pants <_pants@getlantern.org>
2013-10-31 11:52:31 -0400

    closes getlantern/lantern#1098

--- src/main/java/org/lantern/admin/rest/CSRFProtectionFilter.java
+++ src/main/java/org/lantern/admin/rest/CSRFProtectionFilter.java
@@ -21,9 +21,9 @@ import org.lantern.SecurityUtils;
 import com.google.appengine.api.utils.SystemProperty;
 
 /**
- * This filter makes sure that the admin/rest APIs have a CSRF token available
- * and that operations on these APIs that manipulate data (everything but GET
- * and HEAD) always check the CSRF token before processing the operation.
+ * This filter makes sure that operations on the admin/rest APIs that manipulate
+ * data (everything but GET and HEAD) always check the Origin header before
+ * processing the operation to prevent cross-site request forgery.
  */
 public class CSRFProtectionFilter implements Filter {
     private static final transient Logger log = Logger
@@ -33,22 +33,12 @@ public class CSRFProtectionFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
-    /**
-     * See http://docs.angularjs.org/api/ng.$http#description_security-considerations_cross-site-request-forgery-protection
-     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
-        String tokenExpected = AdminServlet.getCsrfToken();
-        // guilty until proven innocent
-        Cookie cookie = new Cookie("XSRF-TOKEN", "invalid");
-        cookie.setMaxAge(0); // delete the cookie
-        cookie.setPath("/admin");
         boolean runningOnGAE = SystemProperty.environment.value() == SystemProperty.Environment.Value.Production;
-        cookie.setSecure(runningOnGAE);
-        resp.addCookie(cookie);
         String method = req.getMethod();
         if (!method.equalsIgnoreCase("GET") && !method.equalsIgnoreCase("HEAD")) {
             final String origin = req.getHeader("Origin");
@@ -59,20 +49,7 @@ public class CSRFProtectionFilter implements Filter {
                 resp.setStatus(403);
                 return;
             }
-            String tokenReceived = req.getHeader("X-XSRF-TOKEN");
-            String tokenReceivedWithoutQuotes = StringUtils.strip(tokenReceived, "\"");
-            // don't short circuit the second string comparison to prevent timing attacks
-            boolean matches = SecurityUtils.constantTimeEquals(tokenExpected, tokenReceived);
-            boolean matchesWithoutQuotes = SecurityUtils.constantTimeEquals(tokenExpected, tokenReceivedWithoutQuotes);
-            if ("lanternctrl".equals(SystemProperty.applicationId.get()) // XXX tokenReceived is null in test controllers; disabling this check until that gets fixed.
-                && !matches && !matchesWithoutQuotes) {
-                log.warning(String.format("Got bad CSRF token: %1$s", tokenReceived));
-                resp.setStatus(403);
-                return;
-            }
         }
-        cookie.setMaxAge(-1); // session cookie - expires when browser closes
-        cookie.setValue(tokenExpected);
         chain.doFilter(request, response);
     }
 
