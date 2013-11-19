81131fae43541a76c47434a24f629f2d91338a9a
Spider Person <aranhoide@gmail.com>
2013-11-19 04:49:57 +0100

    Extract original sender from forwarded emails.
    While the controller will listen for new requests at
    invite@<controller-id>.appspotmail.com, forwarded emails should go
    to forward@<controller-id>.appspotmail.com, so we know to treat
    them differently.
    
    This works for single e-mails forwarded from one's personal inbox.
    Unfortunately, Google Groups strips the original sender's address
    if you forward from the mailing list interface.  Also, digest mode
    is not supported yet.

--- src/main/java/org/lantern/MailHandlerServlet.java
+++ src/main/java/org/lantern/MailHandlerServlet.java
@@ -1,13 +1,19 @@
 package org.lantern;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.Properties;
 
 import javax.mail.Address;
-import javax.mail.MessagingException;
+import javax.mail.Header;
 import javax.mail.internet.InternetAddress;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.mail.Session;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,16 +31,32 @@ public class MailHandlerServlet extends HttpServlet {
 
     private final transient Logger log = Logger.getLogger(getClass().getName());
 
-    /**
-     * Soft sanity check.
-     *
-     * We're not enforcing this but logging warnings if we get email to the
-     * wrong address.
-     */
-    private static final String EXPECTED_PATHINFO
-        = "/invite@" + SystemProperty.applicationId.get() + ".appspotmail.com";
+    private static final Pattern ATTACHED_EMAIL_PATTERN
+        = Pattern.compile("<(.+@.+\\..+)>");
 
-    /** The inviter for all invites triggered by this servlet. */
+    private static String pathInfoFromUsername(String username) {
+        return "/" + username + "@" + SystemProperty.applicationId.get()
+               + ".appspotmail.com";
+    }
+
+    /**
+     * The email at which we're listening for direct invite requests.
+     */
+    private static final String DIRECT_PATHINFO
+        = pathInfoFromUsername("invite");
+
+    /**
+     * The email at which we're listening for forwarded invite requests.
+     *
+     * We use this to process invite requests that predate the deployment of this
+     * servlet.
+     */
+    private static final String FORWARD_PATHINFO
+        = pathInfoFromUsername("forward");
+
+    /**
+     * The inviter for all invites triggered by this servlet.
+     */
     private static final String INVITER = "invite@getlantern.org";
 
     /**
@@ -49,44 +71,67 @@ public class MailHandlerServlet extends HttpServlet {
     public void doPost(final HttpServletRequest req,
                        final HttpServletResponse res)
             throws IOException {
-        if (!EXPECTED_PATHINFO.equals(req.getPathInfo())) {
-            log.warning("Got message to unexpected endpoint: "
-                        + req.getPathInfo());
-        }
-        Address[] from;
+        String sender;
         try {
-            from = new MimeMessage(
-                        Session.getDefaultInstance(new Properties(), null),
-                        req.getInputStream()).getFrom();
+            MimeMessage msg = new MimeMessage(
+                                Session.getDefaultInstance(new Properties(), null),
+                                req.getInputStream());
+            String path = req.getPathInfo();
+            if (DIRECT_PATHINFO.equals(path)) {
+                log.info("Just using sender's email.");
+                sender = extractSenderFromDirectEmail(msg);
+            } else if (FORWARD_PATHINFO.equals(path)) {
+                log.info("Searching for email in forwarded content.");
+                sender = extractSenderFromForwardedEmail(msg);
+            } else {
+                log.severe("Received email at unknown address: " + path);
+                return;
+            }
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
-        if (from == null) {
-            log.warning("Ignoring message with null sender.");
+        if (StringUtils.isEmpty(sender)) {
+            log.severe("Ignoring message with no sender: " + sender);
             return;
         }
-        if (from.length != 1) {
-            log.warning("Got " + from.length + " addresses?");
+        if (shouldIgnore(sender)) {
+            log.info("Ignoring " + sender);
+            return;
+        } else {
+            log.info("Adding invite to " + sender);
+            new Dao().addInvite(INVITER, sender, null);
         }
-        Dao dao = new Dao();
-        for (Address address : from) {
-            String senderEmail = ((InternetAddress) address).getAddress();
-            if (StringUtils.isBlank(senderEmail)) {
-                log.severe("Message from no sender? " + senderEmail);
-            } else {
-                boolean ignore = false;
-                for (String ignorePattern : IGNORE) {
-                    if (senderEmail.matches(ignorePattern)) {
-                        log.info("Ignoring email from " + senderEmail);
-                        ignore = true;
-                        break;
-                    }
-                }
-                if (!ignore) {
-                    log.info("Adding invite to " + senderEmail);
-                    dao.addInvite(INVITER, senderEmail, null);
-                }
+    }
+
+    private String extractSenderFromForwardedEmail(MimeMessage msg)
+            throws MessagingException, IOException {
+        MimeMultipart mmp = (MimeMultipart)msg.getContent();
+        MimeBodyPart bp = (MimeBodyPart)mmp.getBodyPart(0);
+        String content = (String)bp.getContent();
+        Matcher m = ATTACHED_EMAIL_PATTERN.matcher(content);
+        if (!m.find()) {
+            throw new RuntimeException("Couldn't find an email in " + content);
+        }
+        return m.group(1);
+    }
+
+    private String extractSenderFromDirectEmail(MimeMessage msg)
+            throws MessagingException {
+        Address[] from = msg.getFrom();
+        if (from.length != 1) {
+            throw new RuntimeException(
+                    "I don't know what to do with messages with "
+                    + from.length + " senders.");
+        }
+        return ((InternetAddress) from[0]).getAddress();
+    }
+
+    private boolean shouldIgnore(String sender) {
+        for (String ignorePattern : IGNORE) {
+            if (sender.matches(ignorePattern)) {
+                return true;
             }
         }
+        return false;
     }
 }
