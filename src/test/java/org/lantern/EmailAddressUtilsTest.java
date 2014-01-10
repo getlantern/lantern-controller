e69e357ad259203aa7afde91a1137ced2b4a9bef
_pants <_pants@getlantern.org>
2014-01-10 15:03:43 -0500

    allow google apps for domains

--- src/test/java/org/lantern/EmailAddressUtilsTest.java
+++ src/test/java/org/lantern/EmailAddressUtilsTest.java
@@ -12,7 +12,7 @@ public class EmailAddressUtilsTest {
     public void testNormalizedEmail() throws Exception {
         String[] inputs = {
                 "abc@gmail.com",
-                "AbC@gmail.com",
+                "AbC@GMaiL.COM",
                 "a.bc@gmail.com",
                 "a.b.c@gmail.com",
                 "abc+123@gmail.com",
@@ -25,5 +25,9 @@ public class EmailAddressUtilsTest {
             String outputObserved = EmailAddressUtils.normalizedEmail(input);
             assertTrue(StringUtils.equals(outputObserved, outputExpected));
         }
+
+        String googleAppsAddr = "abc@googleappsdomain.net";
+        String transformed = EmailAddressUtils.normalizedEmail(googleAppsAddr);
+        assertTrue(StringUtils.equals(transformed, googleAppsAddr));
     }
 }
