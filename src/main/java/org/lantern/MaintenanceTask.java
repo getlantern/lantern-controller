041ad131d6b3befca5a53a777cd17a189aab86a7
Spider Person <aranhoide@gmail.com>
2014-02-20 01:31:24 +0100

    Some fixes to task to check user certs and auth tokens.
    The attempt to catch deadlineexceedederrors doesn't work, but
    the hack to resume after the last processed email does.  Since
    this is one-shot code, I'll just commit it as is.
    
    Commenting out the call to perform the actual job, so this can
    stay deployed.

--- src/main/java/org/lantern/MaintenanceTask.java
+++ src/main/java/org/lantern/MaintenanceTask.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.apphosting.api.DeadlineExceededException;
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import org.lantern.data.Dao;
@@ -31,11 +33,20 @@ public class MaintenanceTask extends HttpServlet {
         // This space reserved for your hacks.
         // Deploy them, run them, delete/disable them, redeploy with them deleted/disabled.
         // DON'T LEAVE THEM ENABLED, EITHER IN GITHUB OR GAE!
-        checkConfigFolders();
+        //checkConfigFolders(input);
         LanternControllerUtils.populateOKResponse(response, "OK");
     }
 
-    private void checkConfigFolders() {
+    private void checkConfigFolders(String input) {
+
+        String email = null;
+        if (!StringUtils.isBlank(input)
+            && input.matches("\\s*.+@.+\\..+\\s*")) {
+            email = EmailAddressUtils.normalizedEmail(input.trim());
+            log.info("Starting with email: " + email);
+        } else {
+            log.info("Starting from the beginning");
+        }
 
         Dao dao = new Dao();
         int nulls = 0;
@@ -47,67 +58,76 @@ public class MaintenanceTask extends HttpServlet {
         HashSet<String> tokens = new HashSet<String>();
         HashSet<String> certs = new HashSet<String>();
 
-        for (LanternUser user : dao.ofy().query(LanternUser.class)) {
-            log.info("Checking " + user.getId());
-            String configFolder = user.getConfigFolder();
-            if (configFolder == null) {
-                log.info("Null config!");
-                nulls += 1;
-                continue;
-            }
-            boolean goodToken;
-            boolean goodCert;
-            try {
-                URL url = new URL("https://s3-ap-southeast-1.amazonaws.com/lantern-config/"
-                                  + configFolder
-                                  + "/config.json");
-                Map<String, Object> m = new ObjectMapper().readValue(
-                                                url.openStream(), Map.class);
-                List<Map<String, Object>> fbs = (List)m.get("fallbacks");
-                if (fbs.size() != 1) {
-                    throw new RuntimeException("Weird-sized fallback list: " + fbs.size());
+        try {
+            for (LanternUser user : dao.ofy().query(LanternUser.class)) {
+                if (email != null && !email.equals(user.getId())) {
+                    continue;
                 }
-                String authToken = (String)fbs.get(0).get("auth_token");
-                String cert = (String)fbs.get(0).get("cert");
+                email = null;
+                log.info("Checking " + user.getId());
+                String configFolder = user.getConfigFolder();
+                if (configFolder == null) {
+                    log.info("Null config!");
+                    nulls += 1;
+                    continue;
+                }
+                boolean goodToken;
+                boolean goodCert;
+                try {
+                    URL url = new URL("https://s3-ap-southeast-1.amazonaws.com/lantern-config/"
+                                      + configFolder
+                                      + "/config.json");
+                    Map<String, Object> m = new ObjectMapper().readValue(
+                                                    url.openStream(), Map.class);
+                    List<Map<String, Object>> fbs = (List)m.get("fallbacks");
+                    if (fbs.size() != 1) {
+                        throw new RuntimeException("Weird-sized fallback list: " + fbs.size());
+                    }
+                    String authToken = (String)fbs.get(0).get("auth_token");
+                    String cert = (String)fbs.get(0).get("cert");
 
-                if (authToken == null) {
-                    log.info("Null token!");
-                    goodToken = false;
-                } else if (tokens.contains(authToken)) {
-                    log.info("Good token.");
-                    goodToken = true;
-                } else if (tokens.size() < expectedNumberOfFallbacks) {
-                    log.info("Saw token " + authToken + " for the first time.  Adding.");
-                    tokens.add(authToken);
-                    goodToken = true;
-                } else {
-                    log.info("BAD TOKEN: " + authToken);
-                    goodToken = false;
+                    if (authToken == null) {
+                        log.info("Null token!");
+                        goodToken = false;
+                    } else if (tokens.contains(authToken)) {
+                        log.info("Good token.");
+                        goodToken = true;
+                    } else if (tokens.size() < expectedNumberOfFallbacks) {
+                        log.info("Saw token " + authToken + " for the first time.  Adding.");
+                        tokens.add(authToken);
+                        goodToken = true;
+                    } else {
+                        log.info("BAD TOKEN: " + authToken);
+                        goodToken = false;
+                    }
+                    if (cert == null) {
+                        log.info("Null cert!");
+                        goodCert = false;
+                    } else if (certs.contains(cert)) {
+                        log.info("Good cert.");
+                        goodCert = true;
+                    } else if (certs.size() < expectedNumberOfFallbacks) {
+                        log.info("Saw cert " + cert + " for the first time.  Adding.");
+                        certs.add(cert);
+                        goodCert = true;
+                    } else {
+                        log.info("BAD cert: " + cert);
+                        goodCert = false;
+                    }
+                    if (goodToken && goodCert) {
+                        ok += 1;
+                    } else {
+                        fishy += 1;
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
-                if (cert == null) {
-                    log.info("Null cert!");
-                    goodCert = false;
-                } else if (certs.contains(cert)) {
-                    log.info("Good cert.");
-                    goodCert = true;
-                } else if (certs.size() < expectedNumberOfFallbacks) {
-                    log.info("Saw cert " + cert + " for the first time.  Adding.");
-                    certs.add(cert);
-                    goodCert = true;
-                } else {
-                    log.info("BAD cert: " + cert);
-                    goodCert = false;
-                }
-                if (goodToken && goodCert) {
-                    ok += 1;
-                } else {
-                    fishy += 1;
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
             }
+            log.info("Done!");
+        } catch (DeadlineExceededException e) {
+            log.info("Deadline exceeded; summary so far:");
         }
-        log.info("Done! OK: " + ok + "; fishy: " + fishy + "; null: " + nulls
+        log.info("OK: " + ok + "; fishy: " + fishy + "; null: " + nulls
                  + "; certs: " + certs.size() + "; tokens:" + tokens.size());
     }
 }
