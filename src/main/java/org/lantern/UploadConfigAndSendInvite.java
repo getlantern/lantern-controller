829bfdcbbabb1a59f58b1c4f8bf417d4ebb8faa9
Spider Person <aranhoide@gmail.com>
2014-05-17 11:01:05 +0200

    remove unused imports

--- src/main/java/org/lantern/UploadConfigAndSendInvite.java
+++ src/main/java/org/lantern/UploadConfigAndSendInvite.java
@@ -6,9 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.taskqueue.QueueFactory;
-import com.google.appengine.api.taskqueue.TaskOptions;
-
 import org.lantern.data.Dao;
 
 
